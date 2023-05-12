4898041a57d1e9d7c87cb92ee9500cf59f81fbc3
navneetco0 <navneetharsh3@gmail.com>
2023-05-13 05:11:34 +0530

    profilepic fixed

--- src/components/Auth/ProfilePic.js
+++ src/components/Auth/ProfilePic.js
@@ -1,34 +1,35 @@
-import { Box, Center, Circle, Text } from "@chakra-ui/react";
+import { Box, Circle, Image, Text } from "@chakra-ui/react";
 import { Camera, User } from "../../Assets/svgs/Form";
 import "./Auth.css";
 
-export const ProfilePic = ({ ...others }) => {
-    return (
-        <Circle
-            border={"1px solid gray"}
-            w="200px"
-            h="200px"
-            position="relative"
-            overflow={"hidden"}
-            className="profileBox"
-        >
-            <Box w="250px" h="250px" position={"absolute"} color="primary.700">
-                <User />
-            </Box>
-            <Box className="profileCamBox">
-                <Box w="200px" h="200px" className="overlay">
-                    <Camera />
-                    <Text fontSize={"xl"} color="white" fontWeight={"black"}>
-                        Select Profile Picture
-                    </Text>
-                </Box>
-                <input
-                    className="profileInput"
-                    id="profile_pic"
-                    type={'file'}
-                    {...others}
-                />
-            </Box>
-        </Circle>
-    );
+export const ProfilePic = ({ file, ...others }) => {
+  const src = file ? URL.createObjectURL(file) : null;
+  return (
+    <Circle
+      border={"1px solid gray"}
+      w="200px"
+      h="200px"
+      position="relative"
+      overflow={"hidden"}
+      className="profileBox"
+    >
+      <Box w="250px" h="250px" position={"absolute"} color="primary.700">
+        {src ? <Image src={src} alt="profile_pic" /> : <User />}
+      </Box>
+      <Box className="profileCamBox">
+        <Box w="200px" h="200px" className="overlay">
+          <Camera />
+          <Text fontSize={"xl"} color="white" fontWeight={"black"}>
+            Select Profile Picture
+          </Text>
+        </Box>
+        <input
+          className="profileInput"
+          id="profile_pic"
+          type={"file"}
+          {...others}
+        />
+      </Box>
+    </Circle>
+  );
 };
---
+++ src/components/Helper/Crop.js
@@ -0,0 +1,65 @@
+import { Box, Button, Flex, Text } from "@chakra-ui/react";
+import { useCallback, useState } from "react";
+import Cropper from "react-easy-crop";
+import getCroppedImg from "./cropImg";
+import { Close } from "../../Assets/svgs/Form";
+
+export const Crop = ({ profile_pic, setDisplay, handleChange, aspect }) => {
+  const [crop, setCrop] = useState({ x: 0, y: 0 });
+  const [croppedAreaPixels, setCroppedAreaPixels] = useState(null);
+
+  const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
+    setCroppedAreaPixels(croppedAreaPixels);
+  }, []);
+
+  const [value, setValue] = useState(1);
+
+  const onCrop = async () => {
+    const croppedImageUrl = await getCroppedImg(profile_pic, croppedAreaPixels);
+    handleChange({
+      target: {
+        name: "profile_pic",
+        files: [croppedImageUrl],
+      },
+    });
+    // const formData = new FormData();
+    // formData.append("image", croppedImageUrl);
+    // handleChange({ target: { name: "main_profile_pic", value: formData } });
+    setDisplay(false);
+  };
+
+  return (
+    <Box
+      className="container"
+      h={["calc(100vh - 80px)", "80vh"]}
+      w={["100%", "400px"]}
+      m={"auto"}
+      borderRadius={["0px", "10px"]}
+      border={["", "1px solid rgb(186, 186, 186)"]}
+    >
+      <Flex justifyContent={"space-between"} p="5px">
+        <Text>Crop Image</Text>
+        <Box w="24px" onClick={() => setDisplay(false)} cursor="pointer">
+          <Close />
+        </Box>
+      </Flex>
+      <Box pos="relative" h="calc(100% - 160px)" bg="black">
+        <Cropper
+          image={profile_pic}
+          crop={crop}
+          zoom={value}
+          aspect={aspect}
+          onCropChange={setCrop}
+          onCropComplete={onCropComplete}
+          onZoomChange={(e) => setValue(e)}
+        />
+      </Box>
+      <br />
+      <Flex w="fit-content" m="auto">
+        <Button onClick={() => onCrop()} colorScheme="whatsapp">
+          Crop
+        </Button>
+      </Flex>
+    </Box>
+  );
+};
