acc4632e957568af718fa1a52075e0ca66ac2d3a
navneetco0 <navneetharsh3@gmail.com>
2023-05-27 05:11:34 +0530

    new build

--- src/constant/helper.ts
+++ src/constant/helper.ts
@@ -1,3 +1,3 @@
-export const isProduction = true;
+export const isProduction = false;
 const apis = ['http://localhost:4000', 'https://conversa.onrender.com'];
 export const api = apis[Number(isProduction)];
